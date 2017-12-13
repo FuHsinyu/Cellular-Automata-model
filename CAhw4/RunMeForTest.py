30df68ecb6427f60070dd2640c89168d6831bde9
Hsinyu <247047.niuitmo.ru>
2017-12-13 22:11:00 +0300

    Q5 solved

--- CAhw4/RunMeForTest.py
+++ CAhw4/RunMeForTest.py
@@ -29,37 +29,28 @@ systemSize = r * 2 + 1
 initRowLength = 100
 densityListXaxis = list()
 flowListYaxis = list()
-initStateTimes = 10  # R
-densityRange = np.linspace(0, 1, 201)
+densityRange = np.linspace(0, 1, 51)
 rule = 184
-#densityRange = [0.4]
+T = 1000  # T
+initStateTimes = 10  # R
+#densityRange = [0.4,[0.9]] # for Qestion 2
 initRow = [0] * initStateTimes
 flowVehicleList = [0] * initStateTimes
 for density in densityRange:
     for R in range(initStateTimes):
         initRow[R] = initial_row(initRowLength, density)
         model = CAmodel(r, k, rule, initRow[R], systemSize)
-        #def __init__(self, range, base, rule, initRow, systemSize):
         runTime = 0  #run times
         flowVehicle = 0
-        #flowTime = 50
-        while runTime < 1000:  #T
+        while runTime < T:  #T
             currentRow = tuple(model.currentRow)
             runTime += 1
             currentRow = model.step()
             if (currentRow[0] == 1):
                 if (currentRow[1] == 0):
                     flowVehicle += 1
-            #print("current row:", currentRow)
-            #rowDraw.append(currentRow)
         flowVehicleList[R] = flowVehicle
     densityListXaxis.append(density)
     flowListYaxis.append(np.mean(flowVehicle))
 
-#np.floor(np.array(rowDraw))
-#print(densityListXaxis, flowListYaxis)
-#print(rowDraw)
-#testArr = np.array([[1, 1, 1, 2, 3, 4], [1, 2, 3, 4, 5, 6]])
-#print(np.reshape(rowDraw, (100, 102)))
-#model.draw_hw4_bin(rowDraw)
 model.draw_hw4(densityListXaxis, flowListYaxis)
